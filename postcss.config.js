f24b2ad6359e31dccdebee53e75f1530e85b8c11
Jose Carlos Correa <carloscorrea.de@gmail.com>
2018-04-02 18:19:41 -0500

    applyin group styles

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 const tailwindcss = require('tailwindcss');
 module.exports = {
     plugins: [
-        tailwindcss('./node_modules/.bin/tailwind.config.js'),
+        tailwindcss('./tailwind.config.js'),
         require('autoprefixer'),
     ],
 };
--- src/components/TailCard.js
+++ src/components/TailCard.js
@@ -3,21 +3,18 @@ import React from 'react';
 export default ({title, body, image}) => {
   return(
     <div className="
-      shadow-md
-      rounded
-      overflow-hidden
+      card
       sm:flex
-      max-w-md
-      mb-3">
+      ">
           <div style={{ backgroundImage: `url(${image})`}}
-          className="bg-cover bg-center h-48 w-auto sm:h-auto sm:w-64"
+          className="card__image sm:h-auto sm:w-64"
           >
           </div>
           <div className="p-4">
-            <h2 className="font-black mb-3 text-left"> {title} </h2>
-            <p className="text-sm text-grey-dark mb-4 text-justify"> {body}
+            <h2 className="card__title"> {title} </h2>
+            <p className="card__body"> {body}
             </p>
-          <button className="bg-purple hover:bg-purple-light text-white py-3 px-6 font-bold rounded-full mt-1 mb-2">Touch</button>
+          <button className="card__button">Touch</button>
           </div>
         </div>
   )
